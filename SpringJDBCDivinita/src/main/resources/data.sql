da7992f6a6d973bd49674dff73457d16e32b8c9f
DemonMalu <166941913+DemonMalu@users.noreply.github.com>
2025-01-07 18:52:58 +0100

    Delete SpringJDBCDivinita directory

--- SpringJDBCDivinita/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Divinita (divinita_id, nome, razza, abilita, equipaggiamento) VALUES (777, 'Cosmo', 'Divinità Primitiva', 'Onnipresenza', 'Letteralmente qualsiasi cosa o entità');
-INSERT INTO Divinita (divinita_id, nome, razza, abilita, equipaggiamento) VALUES (999, 'Oniro', 'Divinità Primitiva', 'Onniscienza', 'Letteralmente qualsiasi cosa o entità');
-INSERT INTO Divinita(divinita_id, nome, razza, abilita, equipaggiamento) VALUES (888, 'Chaos', 'Divinità Primitiva', 'Onnipotenza', 'Letteralmente qualsiasi cosa o entità');
-
-
